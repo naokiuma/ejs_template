711b98066c2aa81cf626f165c00694cad6c9da9f
kim Nao <sagalumaata@gmail.com>
2022-04-01 14:27:40 +0900

    first commit

---
+++ dest/about.md
@@ -0,0 +1,3 @@
+-- 参考にしたサイト<br>
+構築：https://webdrawer.net/tools/ejs.html<br>
+使い方：https://haniwaman.com/ejs/
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+var ejs = require("gulp-ejs");
+var rename = require("gulp-rename");
+
+//EJS(テンプレートエンジン)
+gulp.task("ejs", function(done) {
+    gulp.src(
+       ["src/ejs/**/*.ejs",'!' + "src/ejs/_*.ejs"] //参照するディレクトリと出力を除外するファイル
+    )
+    .pipe(ejs())
+    .pipe(rename({extname: ".html"})) //拡張子をhtmlに
+    .pipe(gulp.dest("dest/")); //出力先
+
+    done();
+});
