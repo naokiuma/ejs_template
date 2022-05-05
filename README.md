ac2445697dd1c49d15f0a939c7a5159cfbeb5b13
kim Nao <sagalumaata@gmail.com>
2022-05-05 14:41:47 +0900

    watchを追加したい

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 
 -- 便利機能などの情報<br>
 https://qiita.com/y_hokkey/items/31f1daa6cecb5f4ea4c9#%E3%83%AB%E3%83%BC%E3%83%97
+
+-- コマンド<br>
+gulp ejs コンパイル
+
+<br>
+
+--- 両端にアンダーバーをつけると空白が取り除かれる<br>
+<%_ if (meta.css !== '') { _%>
+    <link rel="stylesheet" href="./assetes/css/<%= meta.css %>.css">        
+<%_ } _%>
+<br>
--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,13 @@ gulp.task("ejs", function(done) {
     gulp.src(
        ["src/ejs/**/*.ejs",'!' + "src/ejs/_*.ejs"] //参照するディレクトリと出力を除外するファイル
     )
-    .pipe(ejs())
+    .pipe(ejs(
+        // {rmWhitespace: true},//空白を削除
+    ))
     .pipe(rename({extname: ".html"})) //拡張子をhtmlに
     .pipe(gulp.dest("dest/")); //出力先
 
     done();
 });
+
+
